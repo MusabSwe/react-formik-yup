e6952c0578a9abfb3e25afcb52886a3e3291781b
MUSAB ALQARNI <78758932+MusabSwe@users.noreply.github.com>
2026-05-13 21:25:16 +0300

    Validation LifeCycle (onChange, onMount, and onBlur)

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -4,8 +4,12 @@ import { Row, Col, Form, Button } from 'react-bootstrap';
 
 const LoginSchema = Yup.object().shape({
     email: Yup.string().email("Enter a valid email address").required('Email is required'),
-    password: Yup.string().min(8, 'Password must be at least 8 characters').required('Password is required').matches(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/, 'Password must contain at least one uppercase letter, one lowercase letter, one number and one special character')
+    password: Yup.string()
+        .min(8, 'Password must be at least 8 characters')
+        .required('Password is required')
+        .matches(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/, 'Password must contain at least one uppercase letter, one lowercase letter, one number and one special character')
 });
+
 const Login = () => {
     //  There 2 types of form controlled and uncontrolled
     // 1- controlled form: the form data is handled by the state of the component
@@ -15,6 +19,9 @@ const Login = () => {
             email: '',
             password: ''
         },
+        validateOnBlur: true, // when the user clicks outside the input field it will validate the form and show the error messages if there are any
+        validateOnChange: true, // when the user changes the value of the input field it will validate the form and show the error messages if there are any
+        // validateOnMount: true, // when the component is mounted it will validate the form and show the error messages if there are any
         onSubmit: (values) => {
             alert(JSON.stringify(values, null, 2));
         },
@@ -36,22 +43,34 @@ const Login = () => {
                                 type="text"
                                 placeholder="Enter email"
                                 onChange={formaik.handleChange}
+                                onBlur={formaik.handleBlur}
                                 name='email'
                                 value={formaik.values.email}
                             />
                         </Form.Group>
-                        <div className='text-danger m-1'>{formaik.touched.email && formaik.errors.email ? formaik.errors.email : null}</div>
+                        <div className='text-danger m-1'>
+                            {
+                             formaik.touched.email && formaik.errors.email ?
+                                    formaik.errors.email : null
+                            }
+                        </div>
                         <Form.Group className="mb-3" controlId="formBasicPassword">
                             <Form.Label>Password</Form.Label>
                             <Form.Control
                                 type="password"
                                 placeholder="Password"
                                 onChange={formaik.handleChange}
+                                onBlur={formaik.handleBlur}
                                 name='password'
                                 value={formaik.values.password}
                             />
                         </Form.Group>
-                        <div className='text-danger m-1'>{formaik.touched.password && formaik.errors.password ? formaik.errors.password : null}</div>
+                        <div className='text-danger m-1'>
+                            {
+                                 formaik.touched.password && formaik.errors.password ?
+                                    formaik.errors.password : null
+                            }
+                        </div>
                         <Button variant="primary" type="submit">
                             Submit
                         </Button>
